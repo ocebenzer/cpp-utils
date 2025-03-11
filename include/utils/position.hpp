83cfbbf5450a09ccd67da0ea4ff9b2be75c43026
omer <omer@omer.com>
2025-03-11 16:51:44 +0300

    refactor position as a header-only class

--- include/utils/position.hpp
+++ include/utils/position.hpp
@@ -1,26 +1,93 @@
 #pragma once
 
-#include <string>
 #include <cstdint>
+#include <ostream>
+#include <boost/container_hash/hash.hpp>
 
 namespace ocb {
+    template<typename T = int64_t>
+    struct point {
+        T y;
+        T x;
 
-struct Position {
-    int64_t row;
-    int64_t col;
+        point operator+() const { return *this; }
+        point operator-() const { return {y, x}; }
 
-    bool operator==(const Position&) const = default;
+        point &operator+=(const point &rhs) {
+            y += rhs.y;
+            x += rhs.x;
+            return *this;
+        }
 
-    auto operator<(const Position& rhs) const -> bool;
-    auto operator+=(const Position& rhs) -> Position&;
-};
+        point &operator-=(const point &rhs) {
+            y -= rhs.y;
+            x -= rhs.x;
+            return *this;
+        }
 
+        point &operator*=(const point &rhs) {
+            y *= rhs.y;
+            x *= rhs.x;
+            return *this;
+        }
+
+        point &operator/=(const point &rhs) {
+            y /= rhs.y;
+            x /= rhs.x;
+            return *this;
+        }
+
+        point &operator%=(const point &rhs) {
+            y %= rhs.y;
+            x %= rhs.x;
+            return *this;
+        }
+
+        point operator+(const point &rhs) const { return {y + rhs.y, x + rhs.x}; }
+        point operator-(const point &rhs) const { return {y - rhs.y, x - rhs.x}; }
+        point operator*(const point &rhs) const { return {y * rhs.y, x * rhs.x}; }
+        point operator/(const point &rhs) const { return {y / rhs.y, x / rhs.x}; }
+        point operator%(const point &rhs) const { return {y % rhs.y, x % rhs.x}; }
+
+        /* Comparisons */
+
+        bool operator<(const point &rhs) const {
+            return y < rhs.y || (y == rhs.y && x < rhs.x);
+        }
+
+        bool operator>(const point &rhs) const {
+            return rhs < *this;
+        }
+
+        bool operator<=(const point &rhs) const {
+            return !(*this > rhs);
+        }
+
+        bool operator>=(const point &rhs) const {
+            return !(*this < rhs);
+        }
+
+        bool operator==(const point &rhs) const = default;
+
+        bool operator!=(const point &rhs) const { return !(*this == rhs); }
+
+        /* Stream Operators */
+
+        friend std::ostream &operator<<(std::ostream &o, const point &p) {
+            return o << std::format("({}, {})", p.y, p.x);
+        }
+    };
+
+    template<typename T>
+    point(T...) -> point<T>;
 } // namespace ocb
 
-template <>
-struct std::hash<ocb::Position> {
-    std::size_t operator()(const ocb::Position& pos) const noexcept {
-        const auto str = std::to_string(pos.row) + std::to_string(pos.col);
-        return std::hash<std::string>()(str);
+template<typename T>
+struct std::hash<ocb::point<T> > {
+    std::size_t operator()(const ocb::point<T> &pos) const noexcept {
+        std::size_t seed = 0;
+        boost::hash_combine(seed, pos.y);
+        boost::hash_combine(seed, pos.x);
+        return seed;
     }
 };
